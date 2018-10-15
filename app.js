5e9250bec8722a32b83f5d38cbdc308374e2e431
Treasure <treasure.rose@gmail.com>
2018-10-15 16:05:16 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// We're going to use cats, but this could literally be anything
+  // think about all the stuff on the internet you encounter as a list
+    // shopping on Amazon 
+    // your email inbox 
+    // your Instagram feed
+    // your Twitter feed
+    // Yelp reviews
+      // the list goes on and on! 
+// a way to search for cats 
+  // what do we need the user to do? 
+    // tell us which cat they're searching for 
+      // with a form 
+        // a form field
+        // a button to send the form  
+  // what do we need to do behind the scenes? 
+    // know what the user is searching for 
+      // get the information from the form 
+    // know what the user is searching 
+      // a list of cats 
+        // get the list of cats from the HTML 
+          // what the user is searching will come from many sources, but the main idea
+          // is that we need to have access to the information being searched
+    // now we have cats to search and we know which cat or cats the user is searching for
+        // compare the two pieces of information. 
+        // if the informatiom matches, show the user the correct cat!
+
+// a way to categorize cats by breed 
+// a way to view of a larger photo of a cat 
