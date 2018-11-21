2b6fbbbccf882b4a34fc8491a6c7da68632a0a58
Treasure <treasure.rose@gmail.com>
2018-11-20 20:42:35 -0800

    refactor

--- app.js
+++ app.js
@@ -1,54 +1,63 @@
-const catUL = document.querySelector('ul');
-const filters = document.getElementById('filters'); 
+const gallery = document.getElementById('gallery');
+const menus = document.querySelector('#menus select'); 
 
 let inputField = document.querySelector('input'); 
-const searchButton = document.getElementById('search')
+const search = document.getElementById('search')
 
-// create a function to display cats 
-function printArray(arr){
-  catUL.innerHTML = '';
+// filter cats by color
+function filterCatBy(value){
+  const filteredCats = [];
+    for (let i = 0; i < cats.length; i++) {
+      if(cats[i].color === value){
+        filteredCats.push(cats[i]); 
+      } 
+    }
+  return filteredCats
+}
+
+// Search cats 
+// EXCEEDS ==> search by any property
+function catSearch(searchTerm){
+  const filteredCats = [];
+  for (let i = 0; i < cats.length; i++) {
+    if(cats[i].name.toLowerCase().trim().includes(searchTerm.toLowerCase().trim())){
+      filteredCats.push(cats[i]); 
+    } 
+  }
+  return filteredCats
+}
+
+// Display cats
+function populateGallery(arr){
+  gallery.innerHTML = '';
   for (let i = 0; i < arr.length; i++) {
-    catUL.innerHTML += `
+    gallery.innerHTML += `
       <li>
         <img src="http://placekitten.com/400/400" alt="Placeholder image of kitten">
         <h2>${arr[i].name}</h2>
         <p>Age: ${arr[i].age}</p>
         <p>Breed: ${arr[i].breed}</p>
-        <p> Color: ${arr[i].color}</p>
+        <p>Color: ${arr[i].color}</p>
       </li>
     `
   }
 }
 
-printArray(cats);
+menus.addEventListener("change", (event) =>{
+   let colorOfCat = menus.value;
+  let correctCatArray = filterCatBy(colorOfCat);
 
-// create a function to filter cats 
-function filterCatBy(prop, value){
-    const filteredCats = [];
-    for (let i = 0; i < cats.length; i++) {
-      if(cats[i][prop].toLowerCase().trim() === value.toLowerCase().trim()){
-        filteredCats.push(cats[i]); 
-      } 
-    }
-    return filteredCats
-  }
-
- filters.addEventListener("click", (event) =>{
-  let colorOfCat = event.target.dataset.color;
-  let correctCatArray = filterCatBy("color", colorOfCat);
-
-  if (colorOfCat == "") {
-    printArray(cats);
+  if (colorOfCat == "all") {
+    populateGallery(cats);
   } else {
-    printArray(correctCatArray);
+    populateGallery(correctCatArray);
   }
 
  });
 
-search.addEventListener("click", (event)=> {
-  event.preventDefault();
-  const newArray = filterCatBy("name", inputField.value);
-  printArray(newArray);
-
+search.addEventListener("keyup", (event)=> {
+  const newArray = catSearch(inputField.value);
+  populateGallery(newArray);
 });
 
+populateGallery(cats);
--- catlist.js
+++ catlist.js
@@ -2,49 +2,49 @@ const cats = [
   {
     name: "Susan",
     age: 2,
-    breed: "tuxedo",
-    color: "white"
+    breed: "american shorthair",
+    color: "black"
   },
   {
     name: "Elmer",
     age: 2,
-    breed: "American Shorthair",
-    color: "black"
+    breed: "ragdoll",
+    color: "white"
   },
   {
     name: "Lucy",
     age: 3,
-    breed: "Ragdoll",
+    breed: "tabby",
     color: "tuxedo"
   },
   {
     name: "Arnaud",
     age: 2,
-    breed: "tuxedo",
+    breed: "american shorthair",
     color: "tuxedo"
   },
   {
     name: "Spot",
     age: 2,
-    breed: "tuxedo",
+    breed: "persian",
     color: "white"
   },
   {
     name: "Mr. Bigface",
     age: 3,
-    breed: "black",
+    breed: "persian",
     color: "black"
   },
   {
     name: "Henri",
     age: 2,
-    breed: "tuxedo",
-    color: "white"
+    breed: "tabby",
+    color: "tuxedo"
   },
   {
     name: "Chewy",
     age: 2,
-    breed: "tuxedo",
-    color: "tuxedo"
+    breed: "ragdoll",
+    color: "white"
   }
 ]
